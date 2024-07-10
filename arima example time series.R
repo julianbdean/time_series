02fc6e958275878c7dbbaba213fb420aba831d09
Julian <julian.dean@gmail.com>
2024-07-09 23:45:52 -0400

    Update arima example time series.R

--- arima example time series.R
+++ arima example time series.R
@@ -1,16 +1,31 @@
 # Packages
 library(forecast)
+library(tidyverse)
 
-# data
+# Data
 data(AirPassengers)
 
 # Fit an ARIMA model
 fit <- auto.arima(AirPassengers)
+forecasted_data <- forecast(fit, h = 12)
 
-# Forecast the next 12 months
-forecasted_data <- forecast(fit, h=12)
-summary(fit)
+# Convert to df
+forecast_df <- data.frame(
+  Date = as.Date(time(forecasted_data$mean)),  # Extract dates
+  Point.Forecast = forecasted_data$mean,        # Point forecasts
+  Lo.95 = forecasted_data$lower[, "95%"],       # Lower 95% CI
+  Hi.95 = forecasted_data$upper[, "95%"]        # Upper 95% CI
+)
 
-# Plot
-plot(AirPassengers)
-plot(forecasted_data)
+historical_df <- data.frame(
+  Date = as.Date(time(AirPassengers)),  # Extract dates
+  Passengers = as.numeric(AirPassengers)  # Convert to numeric
+)
+
+# Plotting using ggplot2 with theme_classic
+ggplot() +
+  geom_line(data = historical_df, aes(x = Date, y = Passengers)) +
+  geom_line(data = forecast_df, aes(x = Date, y = Point.Forecast), linetype = "dashed") +
+  geom_ribbon(data = forecast_df, aes(x = Date, ymin = Lo.95, ymax = Hi.95), fill = "grey", alpha = 0.5) +
+  labs(x = "Year", y = "Passengers", title = "Passenger count forecast using ARIMA") +
+  theme_classic()
