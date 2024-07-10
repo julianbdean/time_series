0c144ae258474dd344e6f25f527f439bf3d5473d
Julian <julian.dean@gmail.com>
2024-07-09 23:35:12 -0400

    Update arima example time series.R

--- arima example time series.R
+++ arima example time series.R
@@ -1,26 +1,16 @@
-# Load the package
+# Packages
 library(forecast)
 
-# Load the dataset
+# data
 data(AirPassengers)
-
-# Display the structure of the data
-print(AirPassengers)
-
-# Plot the data
-plot(AirPassengers)
 
 # Fit an ARIMA model
 fit <- auto.arima(AirPassengers)
 
-# Print the summary of the fit
-summary(fit)
-
 # Forecast the next 12 months
 forecasted_data <- forecast(fit, h=12)
+summary(fit)
 
-# Print the forecasted data
-print(forecasted_data)
-
-# Plot the forecasted data
+# Plot
+plot(AirPassengers)
 plot(forecasted_data)
