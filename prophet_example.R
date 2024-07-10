3f6d1448d2ca1ce72ef576b014aab3d576efc8d8
Julian <julian.dean@gmail.com>
2024-07-10 00:50:11 -0400

    Update prophet_example.R

--- prophet_example.R
+++ prophet_example.R
@@ -1,19 +1,31 @@
 # packages
 library(prophet)
+library(tidyverse)
+library(zoo)
 
 # data
 data("AirPassengers")
 
-# wrangle
-air_passengers <- data.frame(
-  ds = as.Date(time(AirPassengers)),
+historical <- data.frame(
+  ds = as.Date(time(AirPassengers)),  # Convert to Date first
   y = as.numeric(AirPassengers)
 )
 
-# fit model
-m <- prophet(air_passengers)
+historical$ds <- as.POSIXct(historical$ds)
+
+# Model
+m <- prophet(historical)
+
 future <- make_future_dataframe(m, periods = 12, freq = 'month')
 forecast <- predict(m, future)
 
 # Plot
-plot(m, forecast)
+ggplot() +
+  geom_line(data = filter(forecast, ds > max(historical$ds)), aes(x = ds, y = yhat, linetype = "Forecast"), color = "blue") +
+  geom_ribbon(data = filter(forecast, ds > max(historical$ds)), aes(x = ds, ymin = yhat_lower, ymax = yhat_upper), 
+              fill = "blue", alpha = 0.3) +
+  geom_line(data = historical, aes(x = ds, y = y, linetype = "Actual"), color = "black") +
+  labs(x = "Date", y = "Passengers", title = "AirPassengers forecast with Prophet") +
+  scale_linetype_manual(name = "Data Type", values = c("Actual" = "solid", "Forecast" = "dashed")) +
+  theme_classic()
+
